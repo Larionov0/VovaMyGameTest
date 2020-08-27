41110043b8b266529e1acc16b35f30ded538f511
Larko_0 <larionov1001@gmail.com>
2020-08-27 20:13:58 +0300

    Core for main menu added

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from time import sleep
 
-for n in range(1, 11):
-    print(10 - n)
-    sleep(1 - 1/n)
 
-print('Привет, игрок!')  # здоровкаемся
+class Menuer:
+    def start(self):
+        for n in range(1, 11):
+            print(10 - n)
+            sleep(1 - 1/n)
+        print('Привет, игрок!')  # здоровкаемся
+        self.main_menu()
+
+    def main_menu(self):
+        while True:
+            print('Главное меню')
+            print('1 - в магазин')
+            print('2 - на заработки')
+            print('3 - на выход')
+            choice = input('Ваш бобер: ')
+            if choice == '1':
+                pass
+            elif choice == '2':
+                pass
+            elif choice == '3':
+                break
+            else:
+                pass
+
+
+menuer = Menuer()
+menuer.start()
